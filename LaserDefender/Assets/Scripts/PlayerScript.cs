0f9283465a997f4b5524e8cdc91ea803bcee6587
nicole118 <nicole118@users.noreply.github.com>
2020-11-06 09:44:11 +0100

    part 2 done

--- LaserDefender/Assets/Scripts/PlayerScript.cs
+++ LaserDefender/Assets/Scripts/PlayerScript.cs
@@ -14,18 +14,26 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        void Move()
-        {
+        Move();
+    }
+    //move the player
+    private void Move()
+    {
+        //var is used as a generic variable. VS will set its type according to its value.
 
-            //var is used as a generic variable. VS will set its type according to its value.
+        //this will get the difference of the x-axis and y-axis that the player moves
+        var deltaX = Input.GetAxis("Horizontal");
+        var deltaY = Input.GetAxis("Vertical");
 
-            // the variable deltaX will be updated with the input that will happen on the x-axis, left and right
-            var deltaX = Input.GetAxis("Horizontal");
-            var newXPos = transform.position.x + deltaX;
+        //the new position will be the current position of x (transform.position.x) + the diffence of x (deltaX)
+        var newXPos = transform.position.x + deltaX;
+        //the new position will be the current position of y (transform.position.y) + the diffence of y (deltaY)
+        var newYPos = transform.position.y + deltaY;
 
-            //the x-position (left or right direction) will be updated according to the variable 'newXPos'
-            //Y position will stay the same
-            transform.position = new Vector2(newXPos, transform.position.y);
-        }
+        //transfrom.position = current position of player
+        //Vectior2 = a point in 2d space   ||   Vector3 = a point in 3d space
+
+        //the x-position and y-positionwill be updated according to these 2 variables.
+        transform.position = new Vector2(newXPos, newYPos);
     }
 }
